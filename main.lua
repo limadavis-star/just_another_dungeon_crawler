8ec60327b7ccc3aebe081a05e532e344d5fc80d9
Davi Lima <davilimadasilva@proton.me>
2026-09-02 15:20:33 -0300

    feat: initialize ECS world

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+local Concord = require("libraries.concord")
+
+local ecsWorld
+
+function love.load()
+    ecsWorld = Concord.world()
+end
+
 function love.draw()
     love.graphics.print("Just Another Dungeon Crawler", 20, 20)
 end
