40a69315aac6730ac979a03e5c812ae0de2879b5
Davi Lima <davilimadasilva@proton.me>
2026-09-02 18:10:43 -0300

    feat: render procedural dungeon cells

--- main.lua
+++ main.lua
@@ -7,11 +7,13 @@ require("src.components.velocity")
 require("src.components.speed")
 require("src.components.controllable")
 
+
 local Player = require("src.entities.player")
 local Dungeon = require("src.world.dungeon")
 local MovementSystem = require("src.systems.movement_system")
 local CircleRenderSystem = require("src.systems.circle_render_system")
 local InputSystem = require("src.systems.input_system")
+local DungeonRenderSystem = require("src.systems.dungeon_render_system")
 
 local ecsWorld
 local dungeon
@@ -20,11 +22,16 @@ function love.load()
     ecsWorld = Concord.world()
 
     -- Input must run before movement so velocity is current for this frame.
-    ecsWorld:addSystems(InputSystem, MovementSystem, CircleRenderSystem)
+    ecsWorld:addSystems(InputSystem, MovementSystem, DungeonRenderSystem, CircleRenderSystem)
 
     Player.create(ecsWorld, 100, 100)
 
     dungeon = Dungeon.generate(60, 32)
+
+    local dungeonRenderSystem =
+        ecsWorld:getSystem(DungeonRenderSystem)
+
+    dungeonRenderSystem:setDungeon(dungeon)
 end
 
 function love.update(dt)
---
+++ src/systems/dungeon_render_system.lua
@@ -0,0 +1,40 @@
+local Concord = require("libraries.concord")
+local Dungeon = require("src.world.dungeon")
+
+local TILE_SIZE = 16
+
+local DungeonRenderSystem = Concord.system({})
+
+function DungeonRenderSystem:setDungeon(dungeon)
+    self.dungeon = dungeon
+end
+
+function DungeonRenderSystem:draw()
+    if not self.dungeon then
+        return
+    end
+
+    -- rotLove coordinates can start at zero, so pairs is used instead of ipairs.
+    for y, row in pairs(self.dungeon.cells) do
+        for x, cell in pairs(row) do
+            if cell == Dungeon.CELL_WALL then
+                love.graphics.setColor(0.22, 0.24, 0.3, 1)
+            else
+                love.graphics.setColor(0.1, 0.11, 0.15, 1)
+            end
+
+            love.graphics.rectangle(
+                "fill",
+                x * TILE_SIZE,
+                y * TILE_SIZE,
+                TILE_SIZE,
+                TILE_SIZE
+            )
+        end
+    end
+
+    -- Restore the default color so this system does not affect later drawing.
+    love.graphics.setColor(1, 1, 1, 1)
+end
+
+return DungeonRenderSystem
